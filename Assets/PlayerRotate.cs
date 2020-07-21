d3e7ec5c43f6f2960a929b57182e37cd797b1065
MJSarvariyan <30783095+MJSarvariyan@users.noreply.github.com>
2020-07-22 02:34:18 +0430

    Player Rotate

---
+++ Assets/PlayerRotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    public float rotateTarget = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rotateTarget -= 90f;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rotateTarget += 90f;
+        }
+    }
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation,
+            Quaternion.Euler(transform.rotation.x, rotateTarget, transform.rotation.z),
+            .3f);
+    }
+}
