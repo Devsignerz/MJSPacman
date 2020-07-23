a5e7db7885e75aed744530e4589cafc3a560c6ec
MJSarvariyan <30783095+MJSarvariyan@users.noreply.github.com>
2020-07-23 22:02:49 +0430

    Better Ghraphic

--- Assets/Scripts/PlayerForward.cs
+++ Assets/Scripts/PlayerForward.cs
@@ -21,26 +21,18 @@ public class PlayerForward : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hitInfu;
 
-        if ( Physics.Raycast(ray, out hitInfu, 100f, mask, QueryTriggerInteraction.Ignore) )
-        {
-            Debug.DrawLine(ray.origin, hitInfu.point, Color.red);
-        } else
-        {
-            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
-        }
+        if (Physics.Raycast(ray, out hitInfu, 100f, mask, QueryTriggerInteraction.Ignore))
+            Debug.DrawLine(ray.origin, hitInfu.point, Color.yellow);
+
         if (hitInfu.distance >= 3f)
-        {
             if (transform.position == targetPosition)
-            {
                 if (transform.forward == Vector3.back ||
-                     transform.forward == Vector3.forward ||
-                     transform.forward == Vector3.left ||
-                     transform.forward == Vector3.right)
+                        transform.forward == Vector3.forward ||
+                        transform.forward == Vector3.left ||
+                        transform.forward == Vector3.right)
                 {
                     targetPosition += transform.forward * 3;
                 }
-            }
-        }
     }
 
     private void FixedUpdate()
