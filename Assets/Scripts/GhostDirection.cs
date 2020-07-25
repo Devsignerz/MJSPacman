59747c880ba3f575542e97b6959287b10a16b244
MJSarvariyan <30783095+MJSarvariyan@users.noreply.github.com>
2020-07-25 19:20:53 +0430

    Better Visuals

--- Assets/Scripts/GhostDirection.cs
+++ Assets/Scripts/GhostDirection.cs
@@ -51,13 +51,10 @@ public class GhostDirection : MonoBehaviour
             if (Vector3.Distance(MoveInDirection(item), target) < Vector3.Distance(MoveInDirection(shortestDirection), target))
                 shortestDirection = item;
 
-            //if (targetFinder.stateSwitcher.state == GhostState.Chase)
-            //{
-                if (Vector3.Distance(MoveInDirection(item) + new Vector3(-54f, 0f, 0f), target) < Vector3.Distance(MoveInDirection(shortestDirection), target))
-                    shortestDirection = item;
-                if (Vector3.Distance(MoveInDirection(item) + new Vector3(54f, 0f, 0f), target) < Vector3.Distance(MoveInDirection(shortestDirection), target))
-                    shortestDirection = item;
-            //}
+            if (Vector3.Distance(MoveInDirection(item) + new Vector3(-54f, 0f, 0f), target) < Vector3.Distance(MoveInDirection(shortestDirection), target))
+                shortestDirection = item;
+            if (Vector3.Distance(MoveInDirection(item) + new Vector3(54f, 0f, 0f), target) < Vector3.Distance(MoveInDirection(shortestDirection), target))
+                shortestDirection = item;
         }
         return shortestDirection;
     }
